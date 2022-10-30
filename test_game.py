e26809ec43cdb5637db56a138e5e94056b61d924
whatf0xx <whatf0xx@gmail.com>
2022-10-30 19:33:03 +0100

    Tests of bot creation with the correct strategy

--- test_game.py
+++ test_game.py
@@ -16,8 +16,25 @@ def test_pkl_basic_creation():
         assert player.input_type == InputType.USER
         assert player.score == 0
 
+
 def test_pkl_with_edit():
     maker = GameMaker(InputType.COM)
     sample_game = maker.new_game("basic_setup.pkl", {"max_score": 2000})
     assert isinstance(sample_game, Game)
     assert sample_game.max_score == 2000
+
+
+def test_bot_creation():
+    maker = GameMaker(InputType.COM)
+    players = {
+        "Harry": InputType.USER,
+        "Leonie": InputType.USER,
+        "Bot 1": (InputType.COM, "RANDOM"),
+        "Bot 2": (InputType.COM, "LAZY-BANK")
+    }
+    sample_game = maker.new_game("basic_setup.pkl", {"players": players})
+    assert isinstance(sample_game, Game)
+    assert sample_game.players[2].input_type == InputType.COM
+    assert sample_game.players[2].strategy == "RANDOM"
+    assert sample_game.players[3].input_type == InputType.COM
+    assert sample_game.players[3].strategy == "LAZY-BANK"
