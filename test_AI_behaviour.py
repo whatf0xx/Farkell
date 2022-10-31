3bcfe9226c0492c3f487528edc703478275b994b
whatf0xx <whatf0xx@gmail.com>
2022-10-31 13:37:48 +0100

    Added test for the print message

--- test_AI_behaviour.py
+++ test_AI_behaviour.py
@@ -1,9 +1,21 @@
 from game import InputType, Player
 
 
-def test_lazy_bank(monkeypatch):
+def test_lazy_bank_score(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: "1 1 1 2 3 4")
     bot = Player("Bot", InputType.USER, InputType.COM, "LAZY-BANK")
     return_value = bot.turn()
     assert bot.score == 300
     assert return_value == 300
+
+
+def test_laz_bank_naming(monkeypatch, capfd):
+    monkeypatch.setattr('builtins.input', lambda _: "1 1 1 2 3 4")
+    bot = Player("Bot", InputType.USER, InputType.COM, "LAZY-BANK")
+    bot.turn()
+
+    def capture_output():
+        yield capfd.readouterr().out
+
+    captured = capture_output()
+    assert next(captured) == "THREE OF A KIND\n"
